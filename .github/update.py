81c1b684b34caacc7048b29f2be692d2e0f80f8c
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-02-03 11:40:13 +0530

    👍

--- .github/update.py
+++ .github/update.py
@@ -41,9 +41,9 @@ async def update_requirements():
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"Succesfully installed {i}")
+            print(f"Succesfully Installed {i}.")
     except Exception as e:
-        print(f"Error while installing requirments {str(e)}")
+        print(f"Error while installing requirments {str(e)}!!")
 
 
 loop = asyncio.get_event_loop()
