b09a194ec697470ad48325a93ca3d63e0dc7e31c
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-02-03 20:46:12 +0530

    👍

--- userbot/Config/catub_config.py
+++ userbot/Config/catub_config.py
@@ -9,3 +9,7 @@ if ENV:
 else:
     if os.path.exists("config.py"):
         from config import Development as Config  # noqa
+
+STRING_SESSION = os.environ.get("STRING_SESSION", None)        
+STRING_SESSION_2 = os.environ.get("STRING_SESSION_2", None)
+STRING_SESSION_3 = os.environ.get("STRING_SESSION_3", None)
