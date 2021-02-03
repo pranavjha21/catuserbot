77bd309be03900592d4a115c66d8868e260bab49
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-02-03 11:23:10 +0530

    👍

--- exampleconfig.py
+++ exampleconfig.py
@@ -2,23 +2,23 @@ from sample_config import Config
 
 
 class Development(Config):
-  #get this values from the my.telegram.org
+  # Get this values from the my.telegram.org
   APP_ID = Your value
   API_HASH = "Your value"
-  # the name to display in your alive message
+  # The name to display in your alive message
   ALIVE_NAME = "Your value"
-  # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+  # Create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
   DB_URI = "Your value"
-  #After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
+  # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
   STRING_SESSION = "Your value"
-  #create a new bot in @botfather and fill the following vales with bottoken and username respectively
+  # Create a new bot in @botfather and fill the following vales with bottoken and username respectively
   TG_BOT_TOKEN_BF_HER = "Your value"
   TG_BOT_USER_NAME_BF_HER = "Your value"
-  #create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
+  # Create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
   PRIVATE_GROUP_BOT_API_ID = -100
-  #command handler 
+  # Command handler 
   COMMAND_HAND_LER = "."
   #sudo enter the id of sudo users userid's in that array
   SUDO_USERS = []
-  # command hanler for sudo
+  # Command hanler for sudo
   SUDO_COMMAND_HAND_LER = "."
