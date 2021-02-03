4939beef96ebc5e0b567dbf77bea72294c68c17c
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-02-03 20:13:51 +0530

    👍

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -54,6 +54,23 @@ try:
         HEROKU_APP = None
 except:
     HEROKU_APP = None
+    
+    # All Clients - 3
+
+if Config.STRING_SESSION:
+    session_name = str(Config.STRING_SESSION)
+    bot = TelegramClient(StringSession(session_name), Config.APP_ID, Config.API_HASH)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, Config.APP_ID, Config.API_HASH)
+if Config.STRING_SESSION_2:
+    client2 =  TelegramClient(StringSession(Config.STRING_SESSION_2), Config.APP_ID, Config.API_HASH)
+else:
+    client2 = None
+if Config.STRING_SESSION_3:
+    client3 =  TelegramClient(StringSession(Config.STRING_SESSION_3), Config.APP_ID, Config.API_HASH)
+else:
+    client3 = None
 
 # Global Configiables
 COUNT_MSG = 0
