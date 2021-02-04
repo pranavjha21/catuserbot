067a31ba8098457b69b9351052b7448ab46c9836
Pranav <77088825+pranavjha21@users.noreply.github.com>
2021-02-04 11:11:40 +0530

    Clona

--- userbot/plugins/clone.py
+++ userbot/plugins/clone.py
@@ -68,7 +68,7 @@ async def _(event):
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID,
-            f"#CLONED\nSuccesfully cloned [{first_name}](tg://user?id={user_id })",
+            f"#CLONED\nHii 😉😀 [{first_name}](tg://user?id={user_id })",
         )
 
 
