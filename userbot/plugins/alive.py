417f2f48be5ad34c027596df09eac4b451654002
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-02-04 18:28:15 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,6 +9,7 @@ from . import StartTime, catversion, get_readable_time, hmention, mention, reply
 
 # backup
 
+chat = await yes.get_chat()
 ALIVE_NAME = Config.ALIVE_NAME
 PINEAPPLE_IMG = Config.ALIVE_PIC
 CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✧✧ PINEAPPLE IS RUNNING SUCCESSFULLY ✧✧"
@@ -16,9 +17,7 @@ EMOJI = Config.CUSTOM_ALIVE_EMOJI or "➥"
 DEFAULT_USER = str(ALIVE_NAME) if ALIVE_NAME else "PineApple"
 on = await bot.send_file(yes.chat_id, file=file1)
 
-global ghanti
-ghanti = bot.uid
-edit_time = 9
+edit_time = 5
 """ =======================CONSTANTS====================== """
 file1 = "https://telegra.ph/file/a0f435a5975c30466e038.jpg"
 file2 = "https://telegra.ph/file/f8c3fefef25b1c99ce1d9.jpg"
