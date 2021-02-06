5a8862a4c37dea2f3e4646b91b85d8c7acd61141
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-02-06 10:46:39 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,7 +1,6 @@
 import time
 from platform import python_version
-import os
-import requests
+from ..Config import Config
 
 from telethon import version
 
@@ -9,27 +8,13 @@ from . import StartTime, catversion, get_readable_time, hmention, mention, reply
 
 # backup
 
-chat = await yes.get_chat()
-ALIVE_NAME = Config.ALIVE_NAME
+
 PINEAPPLE_IMG = Config.ALIVE_PIC
+ALIVE_NAME = Config.ALIVE_NAME
 CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✧✧ PINEAPPLE IS RUNNING SUCCESSFULLY ✧✧"
 EMOJI = Config.CUSTOM_ALIVE_EMOJI or "➥"
-DEFAULT_USER = str(ALIVE_NAME) if ALIVE_NAME else "PineApple"
-on = await bot.send_file(yes.chat_id, file=file1)
 
-edit_time = 5
-""" =======================CONSTANTS====================== """
-file1 = "https://telegra.ph/file/a0f435a5975c30466e038.jpg"
-file2 = "https://telegra.ph/file/f8c3fefef25b1c99ce1d9.jpg"
-file3 = "https://telegra.ph/file/ca1df6456be952d0d5f23.jpg"
-file4 = "https://telegra.ph/file/c98058f2c192e8ce8c5b6.jpg"
-file5 = "https://telegra.ph/file/c866dda4baeacb33752c9.jpg"
-file6 = "https://telegra.ph/file/ca71d325120480ca8871f.jpg"
-file7 = "https://telegra.ph/file/886a2606a353f59e5a30c.jpg"
-file8 = "https://telegra.ph/file/decf1583b93e275041621.jpg"
-""" =======================CONSTANTS====================== """
 
-PINEAPPLE_IMG
 @bot.on(admin_cmd(outgoing=True, pattern="alive$"))
 @bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
 async def amireallyalive(alive):
@@ -39,24 +24,24 @@ async def amireallyalive(alive):
     uptime = await get_readable_time((time.time() - StartTime))
     _, check_sgnirts = check_data_base_heal_th()
     if PINEAPPLE_IMG:
-        cat_caption = f"<b>{CUSTOM_ALIVE_TEXT}</b>\n\n"
-        cat_caption += f"<b>{EMOJI} Boss : {DEFAULT_USER}</b>\n"
-        cat_caption += f"<b>{EMOJI} Uptime :</b> <code>{uptime}</code>\n"
-        cat_caption += (
+        pineapple_caption = f"<b>{CUSTOM_ALIVE_TEXT}</b>\n\n"
+        pineapple_caption += f"<b>{EMOJI} Master : {ALIVE_NAME}</b>\n"
+        pineapple_caption += f"<b>{EMOJI} Uptime :</b> <code>{uptime}</code>\n"
+        pineapple_caption += (
             f"<b>{EMOJI} Python Version :</b> <code>{python_version()}</code>\n"
         )
-        cat_caption += (
+        pineapple_caption += (
             f"<b>{EMOJI} Telethon version :</b> <code>{version.__version__}</code>\n"
         )
-        cat_caption += (
+        pineapple_caption += (
             f"<b>{EMOJI} PineApple Version :</b> <code>{catversion}</code>\n"
         )
-        cat_caption += f"<b>{EMOJI} Database :</b> <code>{check_sgnirts}</code>\n\n"
-        cat_caption += "    <a href = https://github.com/madboy482/PineApple><b>PineApple</b></a> | <a href = https://telegram.me/PineApple_UB><b>Updates</b></a> | <a href = https://telegram.me/PineApple_UB_OnTopic><b>Support</b></a> | <a href = https://telegram.me/PineApple_UB_Spam><b>Spam</b></a>"
+        pineapple_caption += f"<b>{EMOJI} Database :</b> <code>{check_sgnirts}</code>\n\n"
+        pineapple_caption += "    <a href = https://github.com/madboy482/PineApple><b>PineApple</b></a> | <a href = https://telegram.me/PineApple_UB><b>Updates</b></a> | <a href = https://telegram.me/PineApple_UB_OnTopic><b>Support</b></a> | <a href = https://telegram.me/PineApple_UB_Spam><b>Spam</b></a>"
         await alive.client.send_file(
             alive.chat_id,
             PINEAPPLE_IMG,
-            caption=cat_caption,
+            caption=pineapple_caption,
             parse_mode="html",
             reply_to=reply_to_id,
             link_preview=False,
@@ -64,67 +49,20 @@ async def amireallyalive(alive):
         )
         await alive.delete()
     else:
-        await reply(
+        await edit_or_reply(
             alive,
-        cat_caption = f"<b>{CUSTOM_ALIVE_TEXT}</b>\n\n"
-        cat_caption += f"<b>{EMOJI} Boss : {DEFAULT_USER}</b>\n"
-        cat_caption += f"<b>{EMOJI} Uptime :</b> <code>{uptime}</code>\n"
-        cat_caption += (
+            f"<b>{CUSTOM_ALIVE_TEXT}</b>\n\n"
+            f"<b>{EMOJI} Master : {ALIVE_NAME}</b>\n"
+            f"<b>{EMOJI} Uptime :</b> <code>{uptime}</code>\n"
             f"<b>{EMOJI} Python Version :</b> <code>{python_version()}</code>\n"
-        )
-        cat_caption += (
             f"<b>{EMOJI} Telethon version :</b> <code>{version.__version__}</code>\n"
-        )
-        cat_caption += (
             f"<b>{EMOJI} PineApple Version :</b> <code>{catversion}</code>\n"
-        )
-        cat_caption += f"<b>{EMOJI} Database :</b> <code>{check_sgnirts}</code>\n\n"
-        cat_caption += "    <a href = https://github.com/madboy482/PineApple><b>PineApple</b></a> | <a href = https://telegram.me/PineApple_UB><b>Updates</b></a> | <a href = https://telegram.me/PineApple_UB_OnTopic><b>Support</b></a> | <a href = https://telegram.me/PineApple_UB_Spam><b>Spam</b></a>"
-        await alive.client.send_file(
-            alive.chat_id,
-            caption=cat_caption,
+            f"<b>{EMOJI} Database :</b> <code>{check_sgnirts}</code>\n\n"
+            cat_caption += "    <a href = https://github.com/madboy482/PineApple><b>PineApple</b></a> | <a href = https://telegram.me/PineApple_UB><b>Updates</b></a> | <a href = https://telegram.me/PineApple_UB_OnTopic><b>Support</b></a> | <a href = https://telegram.me/PineApple_UB_Spam><b>Spam</b></a>",
             parse_mode="html",
-            reply_to=reply_to_id,
-            link_preview=False,
-            allow_cache=True,
         )
-           await alive.delete()
-            on = await bot.send_file(yes.chat_id, file=file1,caption=cat_caption)
-    
-            await asyncio.sleep(edit_time)
-            ok = await bot.edit_message(yes.chat_id, on, file=file1) 
 
-            await asyncio.sleep(edit_time)
-            ok2 = await bot.edit_message(yes.chat_id, ok, file=file2)
 
-            await asyncio.sleep(edit_time)
-            ok3 = await bot.edit_message(yes.chat_id, ok2, file=file3)
-    
-            await asyncio.sleep(edit_time)
-            ok4 = await bot.edit_message(yes.chat_id, ok3, file=file4)
-    
-            await asyncio.sleep(edit_time)
-            ok5 = await bot.edit_message(yes.chat_id, ok4, file=file5)
-    
-            await asyncio.sleep(edit_time)
-            ok6 = await bot.edit_message(yes.chat_id, ok5, file=file6)
-    
-            await asyncio.sleep(edit_time)
-            ok7 = await bot.edit_message(yes.chat_id, ok6, file=file7)
-
-            await asyncio.sleep(edit_time)
-            ok8 = await bot.edit_message(yes.chat_id, ok7, file=file8)
-    
-            await asyncio.sleep(edit_time)
-            ok9 = await bot.edit_message(yes.chat_id, ok8, file=file4)
-    
-            await asyncio.sleep(edit_time)
-            ok10 = await bot.edit_message(yes.chat_id, ok9, file=file1)
-    
-            await asyncio.sleep(edit_time)
-            ok11 = await bot.edit_message(yes.chat_id, ok10, file=file5)
-            
-            
 @bot.on(admin_cmd(outgoing=True, pattern="ialive$"))
 @bot.on(sudo_cmd(pattern="ialive$", allow_sudo=True))
 async def amireallyalive(alive):
@@ -132,12 +70,12 @@ async def amireallyalive(alive):
         return
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     reply_to_id = await reply_id(alive)
-    cat_caption = f"**PineApple is Up and Running Successfully.**\n"
-    cat_caption += f"**  -Boss :** {DEFAULT_USER}\n"
-    cat_caption += f"**  -Python Version :** `{python_version()}\n`"
-    cat_caption += f"**  -Telethon version :** `{version.__version__}\n`"
-    cat_caption += f"**  -PineApple Version :** `{catversion}`\n"
-    results = await bot.inline_query(tgbotusername, cat_caption)  # pylint:disable=E0602
+    pineapple_caption = f"**PineApple is Up and Running**\n"
+    pineapple_caption += f"**  -Master :** {ALIVE_NAME}\n"
+    pineapple_caption += f"**  -Python Version :** `{python_version()}\n`"
+    pineapple_caption += f"**  -Telethon version :** `{version.__version__}\n`"
+    pineapple_caption += f"**  -PineApple Version :** `{catversion}`\n"
+    results = await bot.inline_query(tgbotusername, pineapple_caption)  # pylint:disable=E0602
     await results[0].click(alive.chat_id, reply_to=reply_to_id, hide_via=True)
     await alive.delete()
 
